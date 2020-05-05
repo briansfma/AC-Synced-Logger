75f535f8c41663b608ab7960476f125fabcd82ad
briansfma <briansfma@gmail.com>
2020-05-05 13:05:24 -0700

    fix: moved redundant operations out of acUpdate to improve speed

--- logger.py
+++ logger.py
@@ -14,7 +14,7 @@ import platform
 import ac
 import acsys
 
-# Fix import path for ctypes (needed for sim_info and screen recording)
+# Fix import path for ctypes (needed for mss and track_times)
 if platform.architecture()[0] == "64bit":
     sysdir = 'apps/python/logger/stdlib64'
 else:
@@ -35,25 +35,31 @@ car_g = 0
 car_speed = 0
 track_times = 0
 perf_delta = 0
-sct = 0
 
+sct = 0
+monitor = 0
 recording = False
-recStatus = 0
+recButton = 0
+seq_num = 0
 frame_num = 0
 
+folder = "apps/python/logger/captures/" # set dir for saving images
+
 def toggle(dummy, var):
-    global recording
+    global recording, seq_num, frame_num, recButton
     
     if recording:
         recording = False
+        ac.setText(recButton, "Start Recording")
     else:
         recording = True
-    
-    ac.setText(recStatus, "Recording? {}".format(recording))
+        seq_num += 1
+        frame_num = 1
+        ac.setText(recButton, "Stop Recording")
 
 def acMain(ac_version):
     global appWindow, car_inputs, car_g, car_speed, track_times, perf_delta
-    global sct, recording, recStatus, frame_num
+    global sct, monitor, recButton
 
     # Start new application in session
     appWindow = ac.newApp("SyncLogger")
@@ -70,23 +76,20 @@ def acMain(ac_version):
     track_times = LaptimeReadout(appWindow, 3, 84)
     perf_delta = DeltaReadout(appWindow, 3, 102)
     
+    # Init screen reader object
     sct = mss.mss()
+    monitor = sct.monitors[2]               # Using 2nd monitor
     
     recButton = ac.addButton(appWindow, "Start Recording")
     ac.setPosition(recButton, 150, 135)
     ac.setSize(recButton, 200, 20)
     ac.addOnClickedListener(recButton, toggle)
     
-    recStatus = ac.addLabel(appWindow, "Recording? {}".format(recording))
-    ac.setPosition(recStatus, 3, 135)
-    
-    frame_num = 1
-    
     return "SyncLogger"
 
 def acUpdate(deltaT):
     global car_inputs, car_g, car_speed, track_times, perf_delta
-    global sct, recording, recStatus, frame_num
+    global sct, folder, monitor, recording, seq_num, frame_num
     
     if recording:
         car_inputs.update()
@@ -96,13 +99,9 @@ def acUpdate(deltaT):
         perf_delta.update(deltaT)
         
         # Grab screen
-        monitor = sct.monitors[2]       # Use the 1st monitor
-
-        # Grab the picture
         im = sct.grab(monitor)          # type: ignore
 
-        # TODO: correct file name LMAO
-        filepath = "apps/python/logger/captures/{:06d}.png".format(frame_num)
-        mss.tools.to_png(im.rgb, im.size, output=filepath)
-        
+        # Save image frame in captures folder
+        filename = "{:02d}_{:06d}.png".format(seq_num, frame_num)
+        mss.tools.to_png(im.rgb, im.size, output=(folder + filename))
         frame_num += 1
